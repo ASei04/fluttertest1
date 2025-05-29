e86e764d8341478d092b609f01688fc13968537f
ASei04 <aseitoku@tdas.co.jp>
2025-05-29 18:39:28 +0900

    ひとまず履歴が分からなくなったので差し替え

--- lib/main.dart
+++ lib/main.dart
@@ -3,57 +3,10 @@ import 'my_home_page.dart'; // 追加
 import 'notification_service.dart';
 
 void main() {
-<<<<<<< HEAD
-  WidgetsFlutterBinding.ensureInitialized();
-  // Initialize the notification service
-  // This is necessary to ensure that the notification service is ready before the app starts
-=======
->>>>>>> 3a366299f6d1f37fc42c3c83a48afcb55a6caa04
   NotificationService().init();
   runApp(const MyApp());
 }
 
-<<<<<<< HEAD
-=======
-class Task {
-  String id;
-  String title;
-  DateTime? dueDate;
-  String memo;
-  bool isCompleted;
-  DateTime? reminderTime;
-
-  Task({
-    required this.id,
-    required this.title,
-    this.dueDate,
-    this.memo = '',
-    this.isCompleted = false,
-    this.reminderTime,
-  });
-
-  Map<String, dynamic> toJson() => {
-    'id': id,
-    'title': title,
-    'dueDate': dueDate?.toIso8601String(),
-    'memo': memo,
-    'isCompleted': isCompleted,
-    'reminderTime': reminderTime?.toIso8601String(),
-  };
-
-  factory Task.fromJson(Map<String, dynamic> json) => Task(
-    id: json['id'],
-    title: json['title'],
-    dueDate: json['dueDate'] != null ? DateTime.parse(json['dueDate']) : null,
-    memo: json['memo'] ?? '',
-    isCompleted: json['isCompleted'] ?? false,
-    reminderTime: json['reminderTime'] != null
-        ? DateTime.parse(json['reminderTime'])
-        : null,
-  );
-}
-
->>>>>>> 3a366299f6d1f37fc42c3c83a48afcb55a6caa04
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
